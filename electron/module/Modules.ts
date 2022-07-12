403b9056947c687eb79c963661dcf830874c8d31
Himyu <max_b98@hotmail.de>
2022-07-12 20:04:31 +0200

    remove data from lobby

--- electron/module/Modules.ts
+++ electron/module/Modules.ts
@@ -21,7 +21,7 @@ export class Modules {
       this.lcu,
       this.server,
       this.menu,
-      ["gameConfig"]
+      /* ["gameConfig"] */
     ))
     this.modules.set("lcu-champ-select", new LCUModule(
       "lcu-champ-select",
