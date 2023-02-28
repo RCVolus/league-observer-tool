8df808e102bf7dc6b48e47e2f6cf48ac9894c467
Himyu <max_b98@hotmail.de>
2023-02-28 19:14:57 +0100

    add version

--- electron/index.ts
+++ electron/index.ts
@@ -59,6 +59,10 @@ if (!gotTheLock) {
     initApp();
     tray = createTray(mainWindow)
 
+    ipcMain.handle('getVersion', () => {
+      return app.getVersion()
+    })
+
     app.on("activate", function () {
       // On macOS it's common to re-create a window in the app when the
       // dock icon is clicked and there are no other windows open.
